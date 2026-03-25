d1ad75f3fb84938841a963065dcbf7973933cacd
dharmapal25 <dharmapalbharati2525@gmail.com>
2026-03-26 01:22:43 +0530

    fix bug

--- Backend/src/Routers/Enquiry.router.js
+++ Backend/src/Routers/Enquiry.router.js
@@ -2,9 +2,9 @@ const express = require("express");
 const enquiryRouter = express.Router();
 
 const { enquiry } = require("../Controllers/enquiry.controller");
-const authMiddleware = require("../middlewares/router.middleware");
+const { authMiddleware } = require("../middlewares/router.middleware");
 
-enquiryRouter.post("/enquiry", authMiddleware, enquiry);
+enquiryRouter.post("/user-enquiry", authMiddleware, enquiry);
 
 module.exports = enquiryRouter;
 
